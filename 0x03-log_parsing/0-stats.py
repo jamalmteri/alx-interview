176aa130d4d1a81dc983dd3adca49dc9ccffdfdd
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2023-09-21 09:04:15 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,59 +1,44 @@
-#!/usr/bin/python3
+!/usr/bin/python3
+"""
+Log parsing
+"""
 import sys
-import signal
 
-# Define a dictionary to store the counts of each status code
-status_code_counts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
 
-# Initialize total file size
-total_file_size = 0
+def print_metrics(file_size, status_codes):
+    """
+    Print metrics
+    """
+    print("File size: {}".format(file_size))
+    codes_sorted = sorted(status_codes.keys())
+    for code in codes_sorted:
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
 
-def print_statistics():
-    global total_file_size
-    print(f"File size: {total_file_size}")
-    for status_code in sorted(status_code_counts.keys()):
-        if status_code_counts[status_code] > 0:
-            print(f"{status_code}: {status_code_counts[status_code]}")
 
-# Function to handle Ctrl+C
-def signal_handler(sig, frame):
-    print_statistics()
-    sys.exit(0)
+codes_count = {'200': 0, '301': 0, '400': 0, '401': 0,
+               '403': 0, '404': 0, '405': 0, '500': 0}
+file_size_total = 0
+count = 0
 
-# Register the signal handler for Ctrl+C
-signal.signal(signal.SIGINT, signal_handler)
-
-line_count = 0
-
-for line in sys.stdin:
-    line = line.strip()
-
-    # Split the line into components using space as a delimiter
-    parts = line.split()
-
-    # Check if the line has the expected format
-    if len(parts) == 7 and parts[5].isdigit() and parts[6].isdigit():
-        status_code = int(parts[5])
-        file_size = int(parts[6])
-
-        # Update total file size
-        total_file_size += file_size
-
-        # Update status code count
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
-
-        line_count += 1
-
-        # Check if it's time to print statistics
-        if line_count % 10 == 0:
-            print_statistics()
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            try:
+                status_code = line.split()[-2]
+                if status_code in codes_count.keys():
+                    codes_count[status_code] += 1
+                # Grab file size
+                file_size = int(line.split()[-1])
+                file_size_total += file_size
+            except Exception:
+                pass
+            # print metrics if 10 lines have been read
+            count += 1
+            if count == 10:
+                print_metrics(file_size_total, codes_count)
+                count = 0
+    except KeyboardInterrupt:
+        print_metrics(file_size_total, codes_count)
+        raise
+   print_metrics(file_size_total, codes_count)
