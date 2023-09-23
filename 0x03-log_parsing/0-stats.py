fa07e0f8d7d94c6b45e476477acac30c812c1034
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2023-09-23 12:13:12 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,39 @@
+#!/usr/bin/python3
+"""
+Log parsing
+"""
+
 import sys
 
-# Initialize variables to store metrics
-total_file_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+if __name__ == '__main__':
 
-try:
-    # Read input line by line from stdin
-    for line in sys.stdin:
-        # Split the line into components
-        parts = line.split()
-        if len(parts) != 7:
-            continue  # Skip lines with incorrect format
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-        # Extract relevant information
-        status_code = int(parts[5])
-        file_size = int(parts[6])
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
-        # Update metrics
-        total_file_size += file_size
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
-
-        line_count += 1
-
-        # Print statistics after every 10 lines
-        if line_count % 10 == 0:
-            print("File size: {}".format(total_file_size))
-            for code in sorted(status_code_counts.keys()):
-                if status_code_counts[code] > 0:
-                    print("{}: {}".format(code, status_code_counts[code]))
-
-except KeyboardInterrupt:
-    # Handle keyboard interruption (CTRL + C)
-    pass
-
-# Print the final statistics
-print("File size: {}".format(total_file_size))
-for code in sorted(status_code_counts.keys()):
-    if status_code_counts[code] > 0:
-        print("{}: {}".format(code, status_code_counts[code]))
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
